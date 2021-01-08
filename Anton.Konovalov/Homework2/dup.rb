db549de24dd3ca2941df1f4624edcf9d2b166b9b
AntonCherkago <antonkonovalov167@gmail.com>
2021-01-08 20:08:38 +0200

    only rails app

--- Anton.Konovalov/Homework2/dup.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-def dup(str)
-  str.collect { |s| s.chars.chunk { |c| c }.collect(&:first).join }
-end
-
-# for example
-p dup(['aaaaabraaaa'])
-p dup(%w[kelless keenness])
